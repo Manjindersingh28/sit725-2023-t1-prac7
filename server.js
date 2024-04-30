099926e4f2f0d772c1158910f9a5a194196bfdfa
Manjinder Singh <mani28au@gmail.com>
2024-04-30 23:32:36 +1000

    added socket io

--- server.js
+++ server.js
@@ -1,43 +1,37 @@
-
 var express = require("express");
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-let http = require('http').createServer(app);
-let io = require('socket.io')(http);
-//const MongoClient = require('mongodb
-
-let collection;
 let Router = require('./routers/router');
-// import controller from './controllers/controller';
+
+// setup db connection
 require('./dbConnection');
 
 var app = express();
+let server = require('http').createServer(app);
+
+// setup socket.io
+let io = require('socket.io')(server);
+io.on('connection', (socket) => {
+console.log('a user connected');
+socket.on('disconnect', () => {
+console.log('user disconnected');
+});
+setInterval(()=>{
+socket.emit('number', parseInt(Math.random()*10));
+}, 1000);
+});
+
 app.use(express.static(__dirname + '/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/cat', Router);
 
-const uri = "mongodb+srv://mani28au:Password28%40@clustersit725-task4.dbkylur.mongodb.net/CatManager"; //updated this line
-var port = process.env.port || 3000;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable Api version
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
-
 const router = express.Router();
 module.exports = router;
 
 app.get('/', (req, res) => {
-        res.render('index.html');
-    });
+    res.render('index.html');
+});
 
 
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log('express server started');
-
 }); 
